60f1adeb5e6b3626be6bd5ea2a457fe66eb5a5ae
Max Vanderbeek <mk27vand@siena.edu>
2025-05-28 17:05:46 -0400

    Finished prompt for Training Data Labeling, outputs to csv file

--- question_eval.py
+++ question_eval.py
@@ -1,16 +1,15 @@
 import unicodedata
 
 # Specify the path to the text file
-file_path = 'Desktop/2024-question-assessment.txt'
+file_path = '2024-question-assessment.txt'
 
 
 
 with open(file_path, 'r', encoding='utf-8') as file:
     for line in file:
         line = line.split("\t")
-        if (line[5] == "4"):
-            with open('Desktop/filtered_questions.txt', 'a', encoding='utf-8') as out_file:
-                out_file.write("Clueweb Tag: " + line[1] + " Rating: " + line[5] + " Question: " + line[7] + "\n")
-
+        if (line[5] == "4" or line[5] == "3"):
+            with open('filtered_questions.txt', 'a', encoding='utf-8') as out_file:
+                out_file.write(line[7])
 
 
